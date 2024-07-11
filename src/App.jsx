d00a4836bccc0935eb7400ad1e76f4343edfd08f
Ihor Nikitin <igreknet@gmail.com>
2024-07-11 11:41:53 +0300

    edit app routes#10

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,33 @@ import CreateOrder, { action as createOrderAction } from './features/order/Creat
 import Order, { loader as orderLoader } from './features/order/Order';
 import { action as updateOrderAction } from './features/order/UpdateOrder';
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
-    path: '/fast-react-pizza',
     element: <AppLayout />,
     errorElement: <Error />,
 
     children: [
       {
-        path: '/fast-react-pizza',
+        path: '/',
         element: <Home />,
       },
       {
-        path: '/fast-react-pizza/menu',
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader, //connect loader to route
         errorElement: <Error />,
       },
       {
-        path: '/fast-react-pizza/cart',
+        path: '/cart',
         element: <Cart />,
       },
       {
-        path: '/fast-react-pizza/order/new',
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: '/fast-react-pizza/order/:orderId',
+        path: '/order/:orderId',
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
