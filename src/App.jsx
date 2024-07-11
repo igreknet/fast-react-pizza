7cfe12d6447c9544b22fb2ceec3819651b1547da
Ihor Nikitin <igreknet@gmail.com>
2024-07-11 11:17:09 +0300

    add basename prop

--- src/App.jsx
+++ src/App.jsx
@@ -48,5 +48,5 @@ const router = createHashRouter([
 ]);
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} basename="/fast-react-pizza/" />;
 }
